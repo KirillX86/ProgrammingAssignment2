87a245a1f5386f5d559d1eb4b68338f44d5207e1
KirillX86 <kdkuzmin@yandex.ru>
2014-08-22 22:17:54 +0400

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,28 @@
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
         set <- function(y) {
+			if(!identical(y, x)){
                 x <<- y
                 s <<- NULL
+			}
         }
         get <- function() x
-        setinvers <- function(solve) s <<- solve
-        getinvers <- function() s
-        list(set = set, get = get,
-             setinvers = setinvers,
-             getinvers = getinvers)
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        cache <- list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+		return(cache)
 }
 
-cacheSolve <- function(x, ...) {
-        s <- x$getinvers()
-        if(!is.null(s)) {
-                message("getting cached data")
-                return(s)
-        }
-        data <- x$get()
-        s <- solve(data, ...)
-        x$setinvers(s)
-        s
+cacheSolve <- function(y, ...) {
+	   s <- y$getsolve()
+	   if(!is.null(s)) {
+			   message("getting cached data")
+			   return(s)
+	   }
+	   data <- y$get()
+	   invS <- solve(data, ...)
+	   y$setsolve(invS)
+	   return(invS)
 }
