9b4f5cfbfed80452df291aa47ea89402e6c01ddc
KirillX86 <kdkuzmin@yandex.ru>
2014-08-23 00:20:56 +0400

    Formatting code

--- cachematrix.R
+++ cachematrix.R
@@ -3,39 +3,39 @@
 ##Return list of functions for setting and getting matrix and it's inverse
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
-		##set function for matrix
+        ##set function for matrix
         set <- function(y) {
-			##if matrix is same its inverse mustn't be reset and recalculated
+            ##if matrix is same its inverse mustn't be reset and recalculated
             if(!identical(y, x)){
                 x <<- y
                 s <<- NULL
             }
         }
-		##get function for matrix
+        ##get function for matrix
         get <- function() x
-		##set function for inverse matrix
+        ##set function for inverse matrix
         setsolve <- function(solve) s <<- solve
-		##get function for inverse matrix
+        ##get function for inverse matrix
         getsolve <- function() s
         cache <- list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
-		return(cache)
+        return(cache)
 }
 
 ##Calculate inverse matrix in case it not cached in argument
 ##Get list with matrix and inverse matrix$
 ##Return inverse matrix
 cacheSolve <- function(y, ...) {
-	    s <- y$getsolve()
-		##if inverse exists it's will not be recalculated
-	    if(!is.null(s)) {
+        s <- y$getsolve()
+        ##if inverse exists it's will not be recalculated
+        if(!is.null(s)) {
             message("getting cached data")
             return(s)
-	    }
-	    data <- y$get()
-		##calculate inverse matrix
-	    invS <- solve(data, ...)
-	    y$setsolve(invS)
-	    return(invS)
+        }
+        data <- y$get()
+        ##calculate inverse matrix
+        invS <- solve(data, ...)
+        y$setsolve(invS)
+        return(invS)
 }
